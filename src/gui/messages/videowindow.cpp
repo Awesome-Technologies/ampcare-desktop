78dc81cf739431d495456825bc895d0331b1ca9f
Michael Albert <michael.albert@awesome-technologies.de>
2019-05-13 10:22:50 +0200

    Persists cookies in the video view to avoid permanent logins
    Change-Id: I90b09ee1f996fd99dec32f1d1cae5e3c49fa72ae

--- src/gui/messages/videowindow.cpp
+++ src/gui/messages/videowindow.cpp
@@ -1,8 +1,10 @@
+#include "configfile.h"
 #include "videowindow.h"
 #include "ui_videowindow.h"
 
 #include <QDesktopWidget>
 #include <QStyle>
+#include <QWebEngineProfile>
 
 namespace OCC {
 
@@ -17,6 +19,15 @@ VideoWindow::VideoWindow(QWidget *parent)
             Qt::AlignCenter,
             QSize(1200, 750),
             qApp->desktop()->availableGeometry()));
+
+    // persist cookies to avoid constant need for login
+    ConfigFile cfg;
+    QString path = cfg.configPath();
+    QWebEngineProfile::defaultProfile()->setPersistentCookiesPolicy(QWebEngineProfile::ForcePersistentCookies);
+    QWebEngineProfile *defaultProfile = QWebEngineProfile::defaultProfile();
+    defaultProfile->setCachePath(cfg.configPath() + "webcache/");
+    defaultProfile->setPersistentStoragePath(cfg.configPath() + "webpersistent/");
+
     QWebEnginePage *page = ui->webEngineView->page();
     QObject::connect(page, &QWebEnginePage::featurePermissionRequested, this, &VideoWindow::slotFeaturePermissionRequested);
 }
